5a8cf67b2be65f465a2dacd86eee719ce921b842
Sikkis <sikkis_vasilis@hotmail.com>
2016-06-20 01:41:04 +0100

    micro assebler alpha version finished

--- assembler/ass_oldversion.py
+++ assembler/ass_oldversion.py
@@ -25,11 +25,26 @@ def read_file(filename):
     global instructions
     global var_counter
     instructions = []
-    variables = [["Zero",0,4]] # name,value,address
-    var_counter = 5
+    variables = [["Zero",0,4]] # [name,value,address]
+    var_counter = 5 # the variable location inside the memory
 
     f = open(filename, 'r')
     for line in f:
+
+        #remove white spaces from line
+        line = line.replace(" ", "")
+
+        #delete whole line comments
+        if line.find("//") >= 0:
+            t = line.split("//")
+            line= t[0]
+
+        #delete block comments
+        if line.find("/*") >= 0:
+            t = line.split("/*")
+            t1 = t[1].split("*/")
+            line = t[0]+t1[1]
+
         #if line is an instruction
         if line.find(";") > 0:
             line = line.strip(";\n").split(",")
@@ -38,13 +53,14 @@ def read_file(filename):
         #else if line is a variable
         else:
             line = line.strip("\n").split(":")
-            #add variable to array and check if its a hex or decimal
+            # add variable to array and check if its a hex or decimal
             if line[1].find("x") > 0 or line[1].find("X") > 0:
                 variables.append([line[0],int(line[1],16),var_counter])
             else:
                 variables.append([line[0],int(line[1],10),var_counter])
-
             var_counter+=1
+    # print instructions
+    # print variables
     f.close()
 
 def write_file():
@@ -54,64 +70,82 @@ def write_file():
     while i<=0xFFFF:
         memory.append([i])
         i+=4
-    #Write the file
-    f = open ("output.txt",'w')
     code_starts = var_counter+ var_counter/4
 
-    #Jump to the  code first line.
-    f.write('0000 00004 00004 00004 '+'{:05x}'.format(code_starts)+"\n")
+    #First line writen :Jump to the codefirst line.
     memory[0].append([4,4,4,code_starts])
 
+    #store variables to memory
     i = 1
     temp = []
     for x in variables:
         temp.append(x[1])
-        print temp
         if x[2]%4 == 3:
             memory[i].append(temp)
             i+=1
             temp = []
-    for x in range(0,var_counter/4):
+    #fill the empty variable slots
+    for x in xrange(0,var_counter/4):
         temp.append(0)
     memory[i].append(temp)
     i+=1
-    print memory[0]
-    print memory[1]
-    print memory[2]
-    print memory[3]
-    #Write the variables
-    i = 4
-    f.write('{:04x}'.format(i)+" ")
-    for x in variables:
-        f.write('{:05x}'.format(x[1])+" ")
-        if x[2]%4 == 3:
-            i+=4
-            f.write("\n"+'{:04x}'.format(i)+" ")
 
-    #fill the empty variable slots
-    for x in range(0,var_counter/4):
-        f.write("00000 ")
-    f.write("\n")
-    i+=4
-
-    #end of writing variables
+    functions = [] # Contains function_name, address
 
     #Write instructions
     for inst in instructions:
+        #check if instruction is a function and save the address
         if inst[0].find(":") > 0:
-            print ("jump"+str(i))
-            jump = inst[0].split(":")
-            if (jump[0]=="code"):
-                print "aa"
-        else:
-            print "normal"
-    while i<=0xFFFF:
-        f.write('{:04x}'.format(i))
-        f.write("\n")
-        i+=4
+            fun = inst[0].split(":")
+            print fun
+            inst[0] = fun[1]
+            print inst
+            functions.append([fun[0],memory[i]]) #push to function name,address
+            print "memory"+ str(memory[i])
 
+        #Checks if instuction is a jump
+        if len(inst) == 4 :
+            #two options jump or shift
+            print "jump"
+
+        #Checks if instuction is simple
+        elif len(inst) == 3:
+            print "simple"
+            print "memory"+ str(memory[i])
+            print "normal"
+            temp = []
+            for x in inst:
+                print x
+                for var in variables:
+                    if x == var[0]:
+                        print str(x) + str(var[0])
+                        print "address "+ ('{:05x}'.format(var[2])+" ")
+                        temp.append(var[2])
+                        break
+            temp.append(memory[i+1][0])
+            memory[i].append(temp)
+            temp = []
+
+        #The syntax of the instruction is wrong better to check at start when reading
+        else:
+            print "Wrong syntax"
+
+        i+=1
+
+    print functions
+    #Write the file
+    f = open ("output.txt",'w')
+    #Write the file
+    for x in xrange(0,9):
+        f.write('{:04x}'.format(memory[x][0])+" ")
+        f.write('{:05x}'.format(memory[x][1][0])+" ")
+        f.write('{:05x}'.format(memory[x][1][1])+" ")
+        f.write('{:05x}'.format(memory[x][1][2])+" ")
+        f.write('{:05x}'.format(memory[x][1][3])+"\n")
     f.close()
 
-
+    print " "
+    for x in xrange(0,10):
+        print memory[x]
 if __name__ == "__main__":
     main(sys.argv[1:])
--- assembler/assembler.py
+++ assembler/assembler.py
@@ -30,20 +30,21 @@ def read_file(filename):
 
     f = open(filename, 'r')
     for line in f:
+
         #remove white spaces from line
         line = line.replace(" ", "")
 
         #delete whole line comments
-        if line.find("//") > 0:
+        if line.find("//") >= 0:
             t = line.split("//")
             line= t[0]
 
         #delete block comments
-        if line.find("/*") > 0:
+        if line.find("/*") >= 0:
             t = line.split("/*")
             t1 = t[1].split("*/")
             line = t[0]+t1[1]
-            print line
+
         #if line is an instruction
         if line.find(";") > 0:
             line = line.strip(";\n").split(",")
@@ -58,17 +59,18 @@ def read_file(filename):
             else:
                 variables.append([line[0],int(line[1],10),var_counter])
             var_counter+=1
-    print instructions
-    print variables
+    # print instructions
+    # print variables
     f.close()
 
 def write_file():
     #create memory blocks format address + [ins,ins,ins,] later
     memory = []
-    i=0
-    while i<=0xFFFF:
-        memory.append([i])
-        i+=4
+    global i
+    x=0
+    while x<=0xFFFF:
+        memory.append([x])
+        x+=4
     code_starts = var_counter+ var_counter/4
 
     #First line writen :Jump to the codefirst line.
@@ -84,25 +86,65 @@ def write_file():
             i+=1
             temp = []
     #fill the empty variable slots
-    for x in range(0,var_counter/4):
+    for x in xrange(0,var_counter/4):
         temp.append(0)
     memory[i].append(temp)
     i+=1
 
+    functions = [] # Contains function_name, address
+
     #Write instructions
     for inst in instructions:
+        #check if instruction is a function and save the address
         if inst[0].find(":") > 0:
-            print ("jump"+str(i))
-            jump = inst[0].split(":")
-            if (jump[0]=="code"):
-                print "aa"
-        else:
-            print "normal"
+            fun = inst[0].split(":")
+            # print fun
+            inst[0] = fun[1]
+            # print inst
+            functions.append([fun[0],memory[i][0]]) #push to function name,address
+            # print "memory"+ str(memory[i][0])
+
+        #handle the first three variable of the instuction and add them to temp
+        # print "memory"+ str(memory[i])
+        # print "normal"
+        temp = []
+        for x in xrange(0,3):
+            # print inst[x]
+            for var in variables:
+                if inst[x] == var[0]:
+                    # print str(x) + str(var[0])
+                    # print "address "+ ('{:05x}'.format(var[2])+" ")
+                    temp.append(var[2])
+                    break
+
+        #Checks if instuction is simple and adds next address
+        if len(inst) == 3:
+            temp.append(memory[i+1][0])
+        #Checks if instuction is a jump and adds name of the jump
+        elif len(inst) == 4:
+            temp.append(inst[3])
+
+        #adds temp array to memory
+        memory[i].append(temp)
+        temp = []
+        i+=1
+
+    #find all jumps and replace them with the addresses
+    for x in xrange(0,i):
+        try:
+            val = int(memory[x][1][3])
+        except ValueError:
+            for fun in functions:
+                if memory[x][1][3] == fun[0]:
+                    memory[x][1][3] = fun[1]
+
+    # add loop at the end of the program.
+    memory[i].append([4,4,4,memory[i][0]])
 
     #Write the file
     f = open ("output.txt",'w')
     #Write the file
-    for x in xrange(0,4):
+    for x in xrange(0,i+1):
         f.write('{:04x}'.format(memory[x][0])+" ")
         f.write('{:05x}'.format(memory[x][1][0])+" ")
         f.write('{:05x}'.format(memory[x][1][1])+" ")
@@ -110,5 +152,8 @@ def write_file():
         f.write('{:05x}'.format(memory[x][1][3])+"\n")
     f.close()
 
+    # print " "
+    # for x in xrange(0,i+1):
+    #     print memory[x]
 if __name__ == "__main__":
     main(sys.argv[1:])
