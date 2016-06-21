acfdbe811106976f6abbbd10019e2f8cc60e7566
Sikkis <sikkis_vasilis@hotmail.com>
2016-06-21 15:46:08 +0100

    error handling and doc

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -4,7 +4,7 @@ import sys, getopt
 
 #The main function take the folowing options and arguments as parameter.
 #-h or --help:
-#   Prints the manual of the program
+#   Prints the manual of the program.
 # -i <filename> or --input <filename> :
 #   Takes an slsx progam and create the memory file. It has the following format:
 #       ADDRESS VALUE VALUE VALUE VALUE
@@ -23,7 +23,7 @@ import sys, getopt
 #       3. Address, four hex digits, representing the 16-bit beginning memory address
 #          offset of the data.
 #       4. Record type, two hex digits, 00 to 05,in the implimentation the 01 and 03 is
-#          used only
+#          used only.
 #       5. Data, six hex digits.
 #       6. Checksum, two hex digits, a computed value that can be used to verify the
 #          record has no errors.
@@ -34,73 +34,113 @@ def main(argv):
         print 'test.py -i <filename>'
         sys.exit(2)
     for opt, arg in opts:
-        #Prints in the standar output the help bar
+        #Prints in the standar output the help bar.
         if opt in ('-h',"--help"):
             print "SLSX  micro assembler:"
             print "usage: assembler.py -i <filename> [-s]"
             print "------------------------------------------------------------"
             print "Options:"
             print " 1. [-h|--help] prints the help menu."
-            print " 2. [-i|--input] takes as an argument a file with SLSX code\n    and creates an output file with the memory addresses."
+            print " 2. [-i|--input] takes a file with SLSX code as an argument\n    and creates an output file with the memory addresses."
             print " 3. [-s|--intel_hex] creates four files in hex_intel format\n    that can be used in the simulator. "
             print "------------------------------------------------------------"
             sys.exit()
-        # Calls the read_file function
+        # Calls the read_file function.
         elif opt in ("-i","--input"):
             read_file(arg)
             memory_creation()
             write_file()
-        #splits memory on 4 hex_intel files
+        #Splits memory on 4 hex_intel files.
         elif opt in ("-s","--intel_hex"):
             print "Creating hex_intel files.."
             intel_hex_converter()
             print "Files were succesfuly created"
 def read_file(filename):
-    #Read the file
+    #Read the file.
     global variables
     global instructions
     global var_counter
     instructions = []
     variables = [["Zero",0,4]] # [name,value,address]
-    var_counter = 5 # the variable location inside the memory
+    var_counter = 5 #The variable location inside the memory.
 
     f = open(filename, 'r')
     for line in f:
 
-        #remove white spaces from line
+        #Remove white spaces from line.
         line = line.strip("\n").replace(" ", "")
 
-        #delete whole line comments
+        #Delete whole line comments.
         if line.find("//") >= 0:
             t = line.split("//")
             line= t[0]
 
-        #delete block comments
+        #Delete block comments.
         if line.find("/*") >= 0:
             t = line.split("/*")
             t1 = t[1].split("*/")
             line = t[0]+t1[1]
 
-        #checks if line contains code or it is empty
+        #Checks if line contains code or it is empty.
         if line:
-            #if line is an instruction add it to array instructions
+            #If line is an instruction add it to array instructions.
             if line.find(";") > 0:
                 line = line.strip(";").split(",")
+
+                #Checks if variables in instruction have correct format. [Error Handling]
+                if len(line) > 4:
+                    print "the "+str(line)+" contains to many variables."
+                    print "Exiting .."
+                    raise SystemExit
+                elif len(line)< 3:
+                    print "the "+str(line)+" contains to few variables."
+                    print "Exiting .."
+                    raise SystemExit
+                #End of checks
+
                 instructions.append(line)
 
-            #else if line is a variable add it to array variables
+            #Else if line is a variable add it to array variables
             else:
                 line = line.split(":")
-                # add variable to array and check if its a hex or decimal
+
+                # [Error Handling]
+                #Check if variable is not a system var.
+                if line[0].startswith("_"):
+                    print "Only system can declare strings starting with '_'"
+                    print "Exiting .."
+                    raise SystemExit
+                for v in variables:
+                    #Check if variable name already exists
+                    if  v[0]==line[0]:
+                        print "Variable already exists."
+                        print "Exiting .."
+                        raise SystemExit
+                #End of [Error Handling]
+
+                #Adds variable to array and check if its a hex or decimal
                 if line[1].find("x") > 0 or line[1].find("X") > 0:
-                    variables.append([line[0],int(line[1],16),var_counter])
+                    #Checks if variable is a number [Error Handling]
+                    try:
+                        variables.append([line[0],int(line[1],16),var_counter])
+                    except ValueError:
+                        print "The variable on line '"+str(line)+"'is not a number."
+                        print "Exiting .."
+                        raise SystemExit
                 else:
-                    variables.append([line[0],int(line[1],10),var_counter])
+                    #Checks if variable is a number [Error Handling]
+                    try:
+                        variables.append([line[0],int(line[1],10),var_counter])
+                    except ValueError:
+                        print "The variable "+str(line)+"is not a number."
+                        print "Exiting .."
+                        raise SystemExit
+
                 var_counter+=1
     f.close()
 
 def memory_creation():
-    #create memory blocks format address + [v,v,v,v]
+    #Create memory blocks format address + [v,v,v,v]
     global memory
     memory = []
     global i
@@ -115,7 +155,7 @@ def memory_creation():
     #First line writen :Jump to the codefirst line.
     memory[0].append([4,4,4,code_starts])
 
-    #store variables to memory
+    #Store variables to memory.
     i = 1
     temp = []
     for x in variables:
@@ -125,51 +165,66 @@ def memory_creation():
             i+=1
             temp = []
 
-    #fill the empty variable slots
+    #Fill the empty variable slots.
     for x in xrange(0,var_counter/4):
         temp.append(0)
     memory[i].append(temp)
     i+=1
 
-    functions = [] # Contains function_name, address
+    functions = [] # Contains function_name, address.
 
-    #Write instructions
+    #Write instructions.
     for inst in instructions:
-        #check if instruction is a function and save the address
+        #Check if instruction is a function and save the address.
         if inst[0].find(":") > 0:
             fun = inst[0].split(":")
             inst[0] = fun[1]
-            functions.append([fun[0],memory[i][0]]) #push to function name,address
+
+            #[Error Handling]
+            #Checks if label does not start with "_".
+            if  fun[0].startswith("_") and ( not fun[0].startswith("_main")):
+                print "Only system can declare labels starting with '_'"
+                print "Exiting .."
+                raise SystemExit
+
+            #Checks for duplicate labels.
+            for t in functions:
+                if fun[0] == t[0]:
+                    print "Duplicate label detected: "+str(fun[0])
+                    print "Exiting .."
+                    raise SystemExit
+            #End of [Error Handling]
+            functions.append([fun[0],memory[i][0]]) #Push to function name,address.
 
         shift_val=0
-        #checks if the commands have a shift
+        #Checks if the commands have a shift.
         if inst[2].find("_shift") > 0:
             inst[2]=inst[2].replace("_shift", "")
             shift_val=0x10000
 
         temp = []
-        # handle the first three variables of the instuction and add them to temp
+        #Handle the first three variables of the instuction and add them to temp.
         for x in xrange(0,3):
             for var in variables:
                 if inst[x] == var[0]:
                     temp.append(var[2])
                     break
 
-        #Checks if instuction is simple and adds next address
+        #Checks if instuction is simple and adds next address.
         if len(inst) == 3:
             temp.append(memory[i+1][0]|shift_val)
-        #Checks if instuction is a jump and adds name of the jump
+        #Checks if instuction is a jump and adds name of the jump.
         elif len(inst) == 4:
             temp.append(inst[3])
 
-        #adds temp array to memory
+        #Adds temp array to memory.
         memory[i].append(temp)
         temp = []
         i+=1
 
-    #find all jumps and replace them with the addresses
+    #Find all jumps and replace them with the addresses.
     for x in xrange(0,i):
-        #check if the address is a string
+        #Check if the address is a string.
         try:
             val = int(memory[x][1][3])
         except ValueError:
@@ -182,13 +237,13 @@ def memory_creation():
                 if memory[x][1][3] == fun[0]:
                     memory[x][1][3] = fun[1]|shift_val
 
-    # add loop at the end of the program.
+    #Adds loop at the end of the program.
     memory[i].append([4,4,4,memory[i][0]])
 
 def write_file():
-    #Write the file
+    #Write the file.
     f = open ("output.txt",'w')
-    #Write the file
+    #Write the file.
     for x in xrange(0,i+1):
         f.write('{:04x}'.format(memory[x][0])+" ")
         f.write('{:05x}'.format(memory[x][1][0])+" ")
@@ -210,12 +265,12 @@ def intel_hex_converter():
     typ  = 0
     address = 0
 
-    #take memory array and create intel_hex files
+    #Take memory array and create intel_hex files.
     for x in xrange(0,i+1):
 
-        #for each memory address write to correct file
+        #For each memory address write to correct file.
         for t in xrange(0,4):
-            #sum is the two's compliment that is insert on the end of each block
+            #sum is the two's compliment that is insert on the end of each block.
             sum = -(size+(address&0xff)+(address>>8&0xff)+typ+(memory[x][1][t]&0xff)+(memory[x][1][t]>>8&0xff)+(memory[x][1][t]>>16&0xff))&0xff;
             f[t].write(":"+'{:02x}'.format(size))
             f[t].write('{:04x}'.format(address))
